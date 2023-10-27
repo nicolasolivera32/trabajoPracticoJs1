1c67c2604e806ec1a61747bd626cbf0703bf726b
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-26 23:58:30 -0300

    se realizo la suma de numero1 y numero2

---
+++ js/ejercicio1.js
@@ -0,0 +1,26 @@
+/*
+Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+
+Suma de numero1 y numero2.
+
+Resta de numero1 y numero2.
+
+Multiplicación de numero1 y numero2.
+
+División de numero1 entre numero2.
+*/
+
+// ingrese los dos números
+
+let numero1 = parseInt(prompt("ingrese el primer número"));
+console.log(numero1);
+
+let numero2 = parseInt(prompt("ingrese el segundo número"));
+console.log(numero2);
+
+// suma de numero1 y numero2
+
+console.log("el resultado es: " + (numero1 + numero2));
+prompt("el resultado es: " + (numero1 + numero2));
+
+
