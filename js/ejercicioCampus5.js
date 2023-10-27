c00a9eca528520fbdc4b8cd0f8d7a111bd839344
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-27 20:12:58 -0300

    se realizó un programa donde se pide dos numeros y se muestra el resultado de la suma de ambos por consola

---
+++ js/ejercicioCampus5.js
@@ -0,0 +1,9 @@
+/* Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+
+let num1 = parseInt(prompt("ingrese el primer número: "));
+let num2 = parseInt(prompt("ingrese el segundo número: "));
+let resultado = num1 + num2;
+
+console.log("El resultado de la suma es: " + resultado);
