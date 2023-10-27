9ee6b928897ce76294cfa2acfd9c9f09e9efba86
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-27 19:38:53 -0300

    se crearon las variables para luego hacer la conversión de Celsius a  Farenheit

---
+++ js/ejercicio3.js
@@ -0,0 +1,9 @@
+/*
+Crea una variable llamada gradosCelsius y asígnale un valor en grados Celsius. Luego, utiliza la fórmula de conversión para calcular los grados Fahrenheit y almacénalos en una variable llamada gradosFahrenheit. Muestra ambos valores en la consola.
+*/
+
+ let gradosCelsius = 40;
+ let gradosFahrenheit =(gradosCelsius * 1.8) + 32;
+
+ console.log(gradosCelsius + " grados Celsius equivalen a: " + gradosFahrenheit + " grados Fahrenheit");
+
