368af20768f88307e8dd32744e04a9053492ccfb
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-27 19:04:15 -0300

    se crearon las variables nombre, apellido y nombreCompleto. Luego se mostraron por consola

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,5 +1,9 @@
 /*Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.*/
 
-let nombre = prompt("ingrese su nombre: ");
-let apellido = prompt("ingrese su apellido: ");
-let nombreCompleto = alert("su nombre completo es: " + nombre + apellido);
+let nombre = "Lionel";
+let apellido = "Messi";
+let nombreCompleto = nombre + apellido;
+
+console.log("Nombre: " + nombre);
+console.log("Apellido: " + apellido);
+console.log("Nombre Completo es: " + nombre + apellido);
