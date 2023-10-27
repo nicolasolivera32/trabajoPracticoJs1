a46686c3f731f2609435cae310459855bf5e4c06
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-27 19:54:51 -0300

    se realizó un programa que muestre por pantalla la suma de dos números

---
+++ js/ejercicioCampus3.js
@@ -0,0 +1,7 @@
+// Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+let num1 = 3;
+let num2 = 5;
+let resultado = num1 + num2
+
+document.write("La suma de 3 + 5 es: " + resultado)
