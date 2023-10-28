4a0092151615cab4821933c8f54faff1d3357f75
Nicolas Olivera <nicolas19olivera@gmail.com>
2023-10-28 17:47:40 -0300

    se realizó un programa donde se pide que ingrese un número y me dice si es divisible por 2, 3, 5, o 7

---
+++ js/ejercicioCampus10.js
@@ -0,0 +1,25 @@
+/*
+Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+Ejemplo: 
+
+input: 20
+Output: El 20 es divisible por 2.
+*/
+
+let numero = parseInt(prompt("ingrese un número"));
+
+if (numero % 2 == 0) {
+  alert("el número " + numero + " es divisible por 2");
+} else {
+  if (numero % 3 == 0) {
+    alert("el número " + numero + " es divisible por 3");
+  } else {
+    if (numero % 5 == 0) {
+      alert("el número " + numero + " es divisible por 5");
+    } else {
+      if (numero % 7 == 0) {
+        alert("el número " + numero + " es divisible por 7");
+      }
+    }
+  }
+}
